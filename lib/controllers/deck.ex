4dfce8f13f62337789e83d5f8c1faff7072f0f6b
David Levine <dlevine@covermymeds.com>
2019-07-24 15:53:46 -0400

    Updates build_deck

--- lib/controllers/deck.ex
+++ lib/controllers/deck.ex
@@ -1,23 +1,22 @@
 defmodule Deck do
   import Card
   @suits ["Spades", "Heart", "Club", "Diamonds"]
-  @rank ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "King", "Queen"]
+  @rank ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen"]
 
   def build_deck do
-    recursive_deck_build(@rank)
+    build_suits(@suits, []) |> Enum.shuffle()
   end
 
-  defp recursive_deck_build([rank_head | rank_tail]) do
-    [
-      %Card{suit: "Spades", rank: rank_head},
-      %Card{suit: "Heart", rank: rank_head},
-      %Card{suit: "Club", rank: rank_head},
-      %Card{suit: "Diamonds", rank: rank_head} |
-       recursive_deck_build(rank_tail)
-    ]
+  defp build_suits([suit_head | suit_tail], results) do
+    build_suits(suit_tail, assign_ranks(suit_head, @rank, results))
   end
-  defp recursive_deck_build([]) do
-    []
+
+  defp build_suits([], results), do: results
+
+  defp assign_ranks(suit, [rank_head | rank_tail], results) do
+    card = %Card{suit: suit, rank: rank_head}
+    assign_ranks(suit, rank_tail, [card | results])
   end
+  defp assign_ranks(_suit, [], results), do: results
 
 end
