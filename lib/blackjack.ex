4941c181cadde4cdf1c4d86f18751827e4ea3373
David Levine <dlevine@covermymeds.com>
2019-07-26 16:37:10 -0400

    Outlines blackjack game logic

--- lib/blackjack.ex
+++ lib/blackjack.ex
@@ -12,7 +12,27 @@ defmodule Blackjack do
       :world
 
   """
-  def hello do
-    :world
+  def launch_game do
+    Console.welcome_player()
+    play_round()
+  end
+
+  def play_round(deck \\ []) do
+    # deal player hand, show dealer's one card
+
+
+    # prompt player to hit/stay
+
+    #if no bust, dealer plays his hand out
+
+    #show dealer result
+
+    # ask to play again or quit
+  end
+
+  def end_game() do
+    Console.end_game()
   end
 end
+
+Blackjack.launch_game()
--- lib/controllers/console.ex
+++ lib/controllers/console.ex
@@ -1,14 +1,22 @@
 defmodule Console do
+  def welcome_player() do
+    IO.puts("Welcome to Blackjack! You will be playing against a dealer which must hit to hard 17!\n")
+  end
+
+  def end_game() do
+    IO.puts("Thanks for playing! Come back to the casino soon!")
+  end
+
   def show_hand(card1, card2, :dealer = _player) do
-    IO.write("Dealer has a #{print_card(card1)}, and a #{print_card(card2)}")
+    IO.puts("Dealer has a #{print_card(card1)}, and a #{print_card(card2)}")
   end
 
   def show_hand(card1, :dealer = _player) do
-    IO.write("Dealer has a #{print_card(card1)}")
+    IO.puts("Dealer has a #{print_card(card1)}")
   end
 
   def show_hand(card1, card2, :player = _player) do
-    IO.write("You have a #{print_card(card1)}, and a #{print_card(card2)}")
+    IO.puts("You have a #{print_card(card1)}, and a #{print_card(card2)}")
   end
 
   def request_player_action do
