ab9e9d8bbdcaa24c97d7f87319a4e96a3684109c
David Levine <dlevine@covermymeds.com>
2019-08-12 16:22:17 -0400

    Adds more console commands

--- lib/controllers/console.ex
+++ lib/controllers/console.ex
@@ -9,25 +9,49 @@ defmodule Console do
     IO.puts("Thanks for playing! Come back to the casino soon!")
   end
 
-  def show_hand([card1, card2], :dealer = _player) do
-    IO.puts("Dealer has a #{print_card(card1)}, and a #{print_card(card2)}")
-  end
-
   def show_hand([card1], :dealer = _player) do
     IO.puts("Dealer has a #{print_card(card1)} showing")
   end
+  def show_hand([card1, card2], :dealer = _player) do
+    score = [card1, card2] |> Card.calculate_hand()
+    IO.puts("Dealer has a #{print_card(card1)}, and a #{print_card(card2)} (#{score})")
+  end
+
 
   def show_hand([card1, card2] = cards, :player = _player) do
     score = cards |> Card.calculate_hand()
+    cards_as_string = calculate_card_array_string([card1, card2])
     IO.puts("You have a #{print_card(card1)}, and a #{print_card(card2)} (#{score})")
   end
 
   def show_hand(card_array, :player = _player) do
     score = card_array |> Card.calculate_hand()
-    cards_as_string = card_array
+    cards_as_string = calculate_card_array_string(card_array)
+    IO.puts("You have: " <> cards_as_string <> " (#{score})")
+  end
+
+  def notify_player_win() do
+    IO.puts("You won! Nice!")
+  end
+
+  def notify_tie() do
+    IO.puts("You both tied!")
+  end
+
+  def notify_player_loss() do
+    IO.puts("Dealer wins! You lost!")
+  end
+
+  def show_hand(card_array, :dealer = _player) do
+    score = card_array |> Card.calculate_hand()
+    cards_as_string = calculate_card_array_string(card_array)
+    IO.puts("Dealer has: " <> cards_as_string <> " (#{score})")
+  end
+
+  defp calculate_card_array_string(card_array) do
+    card_array
       |> Enum.map(fn(card) -> card |> print_card() end)
       |> Enum.join(", ")
-    IO.puts("You have: " <> cards_as_string <> " (#{score})")
   end
 
   def show_hand(card_array, :dealer = _player) do
@@ -44,13 +68,34 @@ defmodule Console do
     end
   end
 
+  def notify_dealer_playing do
+    IO.puts("Dealer playing out hand...")
+    Process.sleep(1000)
+  end
+
+  defp request_player_action(:error = _error) do
+    IO.puts("What? Can you read?. That's not a valid move.")
+    request_player_action()
+  end
   def request_player_action do
     action = IO.gets("Your move: would you like to hit or stay? Enter h for hit, or s for stay\n")
-
     case action do
       "h\n" -> :hit
       "s\n" -> :stay
-      _ -> :error
+      _ -> request_player_action(:error)
+    end
+  end
+
+  defp request_new_round(:error = _error) do
+    IO.puts("What? Can you read?. That's not a valid move.")
+    request_new_round()
+  end
+  def request_new_round() do
+    action = IO.gets("Would you like to play again? (y/n)\n")
+    case action do
+      "y\n" -> true
+      "n\n" -> false
+      _ -> request_new_round(:error)
     end
   end
 
